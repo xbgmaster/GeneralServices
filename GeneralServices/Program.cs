51d033aaeaa6a3a6d68607458cc88ab3a38777e7
xbgmaster <xbgalejandro@gmail.com>
2026-01-21 17:03:06 -0800

    se habilita swagger en produccion

--- GeneralServices/Program.cs
+++ GeneralServices/Program.cs
@@ -13,7 +13,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "GeneralServices API v1");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
