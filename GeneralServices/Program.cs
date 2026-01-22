0a8867330e4be5e8bc6891a9940b565f466d6f4a
xbgmaster <xbgalejandro@gmail.com>
2026-01-21 17:10:00 -0800

    Remove isdevelopment in program.css and httpsredirections

--- GeneralServices/Program.cs
+++ GeneralServices/Program.cs
@@ -10,20 +10,23 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "GeneralServices API v1");
         c.RoutePrefix = "swagger";
     });
-}
+//}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+// optional but important
+app.MapGet("/", () => "GeneralServices API is running");
 
 app.Run();
